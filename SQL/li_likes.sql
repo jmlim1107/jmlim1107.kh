38f9e3a605754fdd682ac976e296a028a05e661f
kimeuna <107541225+kimeunazip@users.noreply.github.com>
2023-03-19 18:25:48 +0900

    Add files via upload

--- SQL/li_likes.sql
+++ SQL/li_likes.sql
@@ -5,11 +5,11 @@ create table li_likes (
 	like_no	number NOT NULL,
     like_date date DEFAULT sysdate NOT NULL,
 	user_no number(10) NOT NULL,
-	class_no number NOT NULL,
+	c_no number NOT NULL,
     
-   	constraint like_no_pk primary key(like_no)
-   	--constraint user_no_fk foreign key(user_no) references li_user(user_no),
-    --constraint class_no_fk foreign key(class_no) references li_class(class_no)
+   	constraint like_no_pk primary key(like_no),
+   	constraint user_no_fk foreign key(user_no) references li_user(user_no),
+    constraint c_no_fk foreign key(c_no) references li_class(c_no)
 );
 
 --drop table li_likes;
@@ -18,7 +18,7 @@ comment on table li_likes is '관심클래스 테이블';
 comment on column li_likes.like_no is '관심클래스번호';
 comment on column li_likes.like_date is '관심클래스등록일자';
 comment on column li_likes.user_no is '회원번호';
-comment on column li_likes.class_no is '클래스이름';
+comment on column li_likes.c_no is '클래스이름';
 
 create sequence li_likes_seq
 start with 1
