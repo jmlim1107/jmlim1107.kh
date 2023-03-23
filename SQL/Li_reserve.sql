7b570dfecc4d8ec95e1fffdaa1fce774871023a1
rudalsss <122061889+rudalsss@users.noreply.github.com>
2023-03-23 09:00:48 +0900

    Update and rename Li_Class, Li_Class_img, Li_episode, Li_reserve.sql to Li_reserve.sql

---
+++ SQL/Li_reserve.sql
@@ -0,0 +1,25 @@
+--예약(Li_reserve)      (update 23.03.14)
+create table li_reserve (
+    r_no number not null,                       --예약번호
+    user_no varchar2 not null,                  --회원번호
+    ep_no number not null,                      --회차번호
+    r_date date default sysdate not null,       --예약일자
+    r_state number default 1 not null,          --예약상태 ( 1:예약중(결제예정) / 2:예약완료(결제완료) / 3: 예약취소(환불) )
+    r_price number not null,                    --예약금액
+    r_cnt number not null,                      --예약인원
+    constraint li_reserve primary key(r_no),
+    constraint li_episode_fk foreign key(user_no) references li_user(user_no),
+    constraint li_episode_fk foreign key(ep_no) references li_episode(ep_no)
+);
+comment on table li_reserve is '클래스 예약 테이블';
+comment on column li_reserve.r_no is '예약 번호';
+comment on column li_reserve.r_date is '예약 일자';
+comment on column li_reserve.r_state is '예약 상태';
+comment on column li_reserve.r_price is '예약 금액';
+comment on column li_reserve.r_cnt is '예약 인원';
+--예약번호
+create sequence li_reserve_seq
+start with 1
+minvalue 1
+nocycle
+nocache;
