7b7ac81b2487943a074b60e58bee77bc38fee941
jmlim1107 <66683160+jmlim1107@users.noreply.github.com>
2023-03-24 12:39:01 +0900

    Update Li_Review.sql

--- SQL/Li_Review.sql
+++ SQL/Li_Review.sql
@@ -2,25 +2,30 @@
 -- drop table Li_review;
 -- 외래키가 잡히지 않은 테스트상태
 
--- 수정사항(23/03/15) : review_name 추가
-
 CREATE TABLE Li_Review(
-    review_no NUMBER(4) NOT NULL,
-    review_name varchar2(60) NOT NULL,
-    review_title varchar2(100) not null,
-    review_content CLOB NOT NULL,
-    review_date DATE NOT NULL,
+    review_no NUMBER(4) null,
+    review_name varchar2(60) null,
+    review_title varchar2(100) null,
+    review_content CLOB null,
+    review_date DATE null,
     review_update DATE NULL,
-    review_status NUMBER(2) NOT NULL,
+    review_status NUMBER(2) null,
     review_img VARCHAR2(50) NULL,
     review_thumb_file VARCHAR2(50) NULL,
+    review_rating number(2) null,
+    review_recommend varchar2(40) null,
+    -- review_rating_total varchar2(30),
     
     -- 추후 변경예정(외래키)
+    r_no number(4),
+    history_no number(4),
     user_img varchar2(50) null,
-    c_no NUMBER(8) NOT NULL,
-    user_no NUMBER(8) NOT NULL,
-    c_name varchar2(50) NOT NULL
-    -- CONSTRAINT fk_c_no FOREIGN KEY(review_c_no) REFERENCES li_class (c_no),   -- 클래스 번호(클래스 테이블 외래키)
+    review_c_no NUMBER(8) null,
+    user_no NUMBER(8) NULL,
+    c_name varchar2(50) null,
+    c_no number(10) null,
+    
+    -- CONSTRAINT review_c_no FOREIGN KEY(review_c_no) REFERENCES Li_class (c_no)  -- 클래스 번호(클래스 테이블 외래키)
     -- CONSTRAINT fk_user_no FOREIGN KEY(review_user_no) REFERENCES li_user (user_no)   -- 회원 번호  (회원 테이블 외래키)
 );
 
@@ -36,14 +41,20 @@ COMMENT ON COLUMN Li_Review.review_update is '후기 수정일';
 COMMENT ON COLUMN Li_Review.review_status is ' 후기 상태(0,1)';
 COMMENT ON COLUMN Li_Review.review_img is '후기 사진명';
 COMMENT ON COLUMN Li_Review.review_thumb_file is '후기 썸네일명';
+COMMENT ON COLUMN Li_Review.review_rating is '후기 별점';
+COMMENT ON COLUMN Li_Review.review_recommend is '후기 추천';
+
 
 -- 추후 변경예정(외래키)
+COMMENT ON COLUMN Li_Review.r_no is '수강내역 글번호';
+COMMENT ON COLUMN Li_Review.history_no is '수강내역 글번호';
 COMMENT ON COLUMN Li_Review.user_img is '유저 프로필 사진';
 COMMENT ON COLUMN Li_Review.c_name is '클래스 명';
 COMMENT ON COLUMN Li_Review.c_no is '클래스 번호';
 COMMENT ON COLUMN Li_Review.user_no is '회원번호';
 
 
+ DROP SEQUENCE Li_review_seq;
 -- Review 테이블 시퀀스
 CREATE SEQUENCE Li_review_seq
     INCREMENT BY 1
@@ -54,6 +65,16 @@ CREATE SEQUENCE Li_review_seq
     NOCACHE
     NOORDER;
 
--- Test Insert
-INSERT INTO LI_REVIEW(REVIEW_NO, REVIEW_NAME, REVIEW_TITLE, REVIEW_CONTENT, REVIEW_DATE, REVIEW_UPDATE, REVIEW_STATUS, C_NO, USER_NO, USER_IMG, C_NAME)
-VALUES(Li_review_seq.nextval, '임지민', '후기제목' , '후기 내용', sysdate, NULL , 0, 12345678, 12345678, NULL, '클래스명');
+-- Test Insert(더미 데이터)
+create or replace procedure reviewProcedure
+is
+begin
+    for cnt in 1..30 loop
+        insert into Li_review (review_no, review_title, review_content, review_date, review_recommend, review_rating, c_no)
+        values(li_review_seq.nextval, '1', '1', sysdate, 'no' , 2, '1234');
+    end loop;
+    commit;
+end;
+/
+show error;
+EXECUTE reviewProcedure;
