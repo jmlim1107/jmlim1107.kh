5cd84f5a3c2872c4cf06d6a30ffac740938dcef7
kimeuna <107541225+kimeunazip@users.noreply.github.com>
2023-03-29 21:41:12 +0900

    테이블 sql 새로 올립니다.

---
+++ LiClassProject/라이클래스프로젝트 테이블SQL/3-1.li_user.sql
@@ -0,0 +1,23 @@
+--Àº¾Æ
+insert into li_user(user_no,user_email,user_pw,user_name,user_tel,user_type)
+values('9205045442','kimeunazip@naver.com','1234','±èÀº¾Æ','01012341234','0');
+
+--¹Î¼·
+insert into li_user(user_no,user_email,user_pw,user_name,user_tel,user_type)
+values('2303290625','ics9133@gmail.com','1234','¹®¹Î¼·','01012341234','0');
+
+--¿õ¹è
+insert into li_user(user_no,user_email,user_pw,user_name,user_tel,user_type)
+values('2303290612','dndqo49@naver.com','1234','Àü¿õ¹è','01095990612','0');
+
+--¼Ò¿µ
+insert into li_user(user_no,user_email,user_pw,user_name,user_tel,user_type)
+values('2303290722','wjdthdud13@naver.com','1234','Á¤¼Ò¿µ','01012341234','0');
+
+--Áö¹Î
+insert into li_user(user_no,user_email,user_pw,user_name,user_tel,user_type)
+values('2001110723','jmlim1107@naver.com','1234','ÀÓÁö¹Î','01012341234','0');
+
+--°æ¹Î
+insert into li_user(user_no,user_email,user_pw,user_name,user_tel,user_type)
+values('2302282828','linda284@naver.com','0000','ÀÌ°æ¹Î','01017441744','0');
---
+++ LiClassProject/라이클래스프로젝트 테이블SQL/구버전SQL/li_user.sql
@@ -0,0 +1,70 @@
+-- 3. li_user
+
+-- 사용자(li_user) 
+-- (update 23.03.16 - 회원번호 랜덤생성함수 추가)  
+-- (update 23.03.22 - 회원프로필사진 default img) 
+-- (update 23.03.29 - 코멘트) 
+
+create table li_user (
+	user_no	number NOT NULL,
+	user_email varchar2(100) NOT NULL,
+	user_pw	varchar2(255) NOT NULL,
+	user_name varchar2(30) NOT NULL,
+    
+	user_tel varchar2(30),
+    user_img	clob DEFAULT 'default-profile.png' NOT NULL,
+    user_point number DEFAULT 0,
+	user_regdate date DEFAULT sysdate NOT NULL,
+    user_update date DEFAULT sysdate NOT NULL,
+	user_unregdate	date,
+	user_status	number	DEFAULT 0 NOT NULL,
+    user_type number  DEFAULT 0 NOT NULL ,
+    
+   	constraint li_user_pk primary key(user_no),
+   	constraint li_user_uk unique(user_email)
+);
+
+--drop table li_user;
+    
+comment on table li_user is '회원 테이블';
+comment on column li_user.user_no is '회원번호';
+comment on column li_user.user_email is '회원이메일';
+comment on column li_user.user_pw is '회원비밀번호';
+comment on column li_user.user_name is '회원이름';
+
+comment on column li_user.user_tel  is '회원연락처';
+comment on column li_user.user_img is '회원프로필사진';
+comment on column li_user.user_point is '포인트';
+
+comment on column li_user.user_regdate is '회원가입일자';
+comment on column li_user.user_update is '회원수정일자';
+comment on column li_user.user_regdate is '회원탈퇴일자';
+comment on column li_user.user_status is '회원계정상태';
+comment on column li_user.user_type is '회원계정종류'; -- 0:이메일 1:카카오 2:네이버
+
+--회원번호 생성 함수
+CREATE OR REPLACE EDITIONABLE FUNCTION USER_NO_FUNC (sysdate IN date)
+RETURN number IS
+user_no number;
+BEGIN 
+  IF sysdate IS NULL OR sysdate = NULL THEN
+    user_no := '0000000000';
+  ELSE 
+      SELECT to_char(sysdate,'YYMMDD') || ROUND(DBMS_RANDOM.VALUE (1000, 9999))
+      INTO user_no 
+      FROM dual;
+  END IF;
+  RETURN user_no;
+EXCEPTION 
+  WHEN OTHERS THEN
+    RETURN '0000000000';
+END user_no_func;
+
+/
+
+--샘플 데이터
+insert into li_user(user_no,user_email,user_pw,user_name,user_tel,user_img,user_type)
+values( user_no_func(sysdate),'test1234@gmail.com','1234','test','01012341234','1.jpg','0');
+
+insert into li_user(user_no,user_email,user_pw,user_name,user_tel,user_type)
+values( user_no_func(sysdate),'test0000@gmail.com','1234','test','01012341234','0');
